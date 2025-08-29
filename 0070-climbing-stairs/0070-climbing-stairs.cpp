b3f6e2219f96b24519d66587829364453f1c247b
Unnati Saini <115027644+unnatissaini@users.noreply.github.com>
2025-08-29 12:35:17 +0530

    0070-climbing-stairs 08-29-2025 Easy 0 ms (100%) 7.6 MB (99.98%)

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector <int> dp(n+1,0);
-        dp[0]=1;
-        dp[1]=1;
+        if ( n==0|| n==1) return n;
+        int prev2=1;
+        int prev=1;
+        int curr=0;
         for( int i=2;i<=n; i++)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+            
         }
-        return dp[n];
+        return curr;
     }
 };
